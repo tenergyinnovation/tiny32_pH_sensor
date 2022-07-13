627da9d1db2b2e797acb74753adcc1393250d1e3
Uten Boonliam <45764087+tenergyinnovation@users.noreply.github.com>
2022-07-13 20:31:04 +0700

    Initial project

---
+++ README.md
@@ -0,0 +1,23 @@
+# tiny32_pH_sensor
+Example code tiny32_v3 interface with DFRobot’s Gravity: Analog pH meter V2 
+
+Original code from
+https://github.com/DFRobot/DFRobot_PH
+
+
+*** Before run this example, you must install tiny32 library first at below link ***
+
+1) git clone https://github.com/tenergyinnovation/tiny32_v3.git
+
+2) git clone https://github.com/adafruit/Adafruit_ADS1X15.git
+
+3) git clone https://github.com/adafruit/Adafruit_BusIO.git
+
+
+*** Additional Information ***
+https://www.cybertice.com/product/854/ph-sensor-arduino-analog-ph-meter-%E0%B9%80%E0%B8%8B%E0%B9%87%E0%B8%99%E0%B9%80%E0%B8%8B%E0%B8%AD%E0%B8%A3%E0%B9%8C%E0%B8%A7%E0%B8%B1%E0%B8%94%E0%B8%84%E0%B9%88%E0%B8%B2-ph-%E0%B8%82%E0%B8%AD%E0%B8%87%E0%B8%99%E0%B9%89%E0%B8%B3
+
+https://wiki.dfrobot.com/Gravity__Analog_pH_Sensor_Meter_Kit_V2_SKU_SEN0161-V2
+
+
+
---
+++ src/tiny32_pH_sensor.h
@@ -0,0 +1,82 @@
+/***********************************************************************
+ * File         :     tiny32_pH_sensor.h
+ * Description  :     Class for tiny32 library for Gravity: Analog pH Sensor / Meter Kit V2, SKU: SEN0161-V2
+ * Author       :     Tenergy Innovation Co., Ltd.
+ * Date         :     13 July 2022
+ * website      :     http://www.tenergyinnovation.co.th
+ * Email        :     uten.boonliam@innovation.co.th
+ * TEL          :     089-140-7205
+ ***********************************************************************/
+/*
+ * **** Original code from *****
+ * @brief Arduino library for Gravity: Analog pH Sensor / Meter Kit V2, SKU: SEN0161-V2
+ * @copyright   Copyright (c) 2010 DFRobot Co.Ltd (http://www.dfrobot.com)
+ * @license     The MIT License (MIT)
+ * @author [Jiawei Zhang](jiawei.zhang@dfrobot.com)
+ * @version  V1.0
+ * @date  2018-11-06
+ * @url https://github.com/DFRobot/DFRobot_PH
+ */
+
+#ifndef _TINY32_PH_SENSOR_
+#define _TINY32_PH_SENSOR_
+
+#if ARDUINO >= 100
+#include "Arduino.h"
+#else
+#include "WProgram.h"
+#endif
+
+#define ReceivedBufferLength 10  //length of the Serial CMD buffer
+
+class tiny32_pH_sensor
+{
+public:
+  tiny32_pH_sensor();
+  ~tiny32_pH_sensor();
+  /**
+   * @fn calibration
+   * @brief Calibrate the calibration data
+   *
+   * @param voltage     : Voltage value
+   * @param temperature : Ambient temperature
+   * @param cmd         : enterph -> enter the PH calibration mode
+   * @n                   calph   -> calibrate with the standard buffer solution, two buffer solutions(4.0 and 7.0) will be automaticlly recognized
+   * @n                   exitph  -> save the calibrated parameters and exit from PH calibration mode
+   */
+  void    calibration(float voltage, float temperature,char* cmd);  //calibration by Serial CMD
+  void    calibration(float voltage, float temperature);
+  /**
+   * @fn readPH
+   * @brief Convert voltage to PH with temperature compensation
+   * @note voltage to pH value, with temperature compensation
+   *
+   * @param voltage     : Voltage value
+   * @param temperature : Ambient temperature
+   * @return The PH value
+   */
+  float   readPH(float voltage, float temperature); 
+  /**
+   * @fn begin
+   * @brief Initialization The Analog pH Sensor
+   */
+  void begin();
+
+private:
+    float  _phValue;
+    float  _acidVoltage;
+    float  _neutralVoltage;
+    float  _voltage;
+    float  _temperature;
+
+    char   _cmdReceivedBuffer[ReceivedBufferLength];  //store the Serial CMD
+    byte   _cmdReceivedBufferIndex;
+
+private:
+    boolean cmdSerialDataAvailable();
+    void    phCalibration(byte mode); // calibration process, wirte key parameters to EEPROM
+    byte    cmdParse(const char* cmd);
+    byte    cmdParse();
+};
+
+#endif
